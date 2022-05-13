0e06a2752a68ede7d08e3704c2ff7ceddee2bb92
Marcus <Marcus.welhi@gmail.com>
2022-05-13 15:14:27 -0300

    first commit

---
+++ aula.js
@@ -0,0 +1,12 @@
+var nome1= "Jao"
+console.log(nome1)
+let nome2 = "Tiago"
+console.log(nome2) 
+const nome3 = "Marcus"
+console.log(nome3)
+
+nome1 = "Jailson"
+console.log(nome1)
+nome2 = "pepe"
+console.log(nome2)
+//nome3 = "Eita"
---
+++ funcao.js
@@ -0,0 +1,18 @@
+//function boasVindas(){
+    //console.log("Boas vindas")
+
+//boasVindas()
+
+function boasVindas2(nome,sobrenome){
+    console.log(`seja bem vindo,${nome}${sobrenome}`)
+}
+
+boasVindas2("Marquinhos","Vininhos")
+
+
+function soma(a,b){
+    return a+b
+}
+
+const resultado = soma(34,53)
+console.log(resultado)
